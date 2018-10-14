9aa391e7d1506d3cacf82681a1bc35ae7b96cc63
GPrajwal12 <praj12121998@gmail.com>
2018-10-15 00:37:51 +0530

    home page added

---
+++ services.php
@@ -0,0 +1,419 @@
+<!--
+	Author: W3layouts
+	Author URL: http://w3layouts.com
+	License: Creative Commons Attribution 3.0 Unported
+	License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title> Intrend Interior Category Flat Bootstrap Responsive Website Template | Services : W3layouts</title>
+	
+	<!-- Meta tag Keywords -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta name="keywords" content="Intrend Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+	Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<script type="application/x-javascript">
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!--// Meta tag Keywords -->
+		
+	<!-- css files -->
+	<link rel="stylesheet" href="css_home/bootstrap.css"> <!-- Bootstrap-Core-CSS -->
+	<link rel="stylesheet" href="css_home/style.css" type="text/css" media="all" /> <!-- Style-CSS --> 
+	<link rel="stylesheet" href="css_home/fontawesome-all.css"> <!-- Font-Awesome-Icons-CSS -->
+	<!-- //css files -->
+	
+	<!-- web-fonts -->
+	<link href="//fonts.googleapis.com/css?family=Poiret+One&amp;subset=cyrillic,latin-ext" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+	<!-- //web-fonts -->
+	
+</head>
+
+<body>
+
+<!-- banner -->
+<div class="inner-page-banner" id="home"> 	   
+	<!--Header-->
+	<header>
+		<div class="container agile-banner_nav">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				
+				<h1><a class="navbar-brand" href="home.php">In <span class="display"> Trend</span></a></h1>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="about.php">About</a>
+						</li>
+						<li class="nav-item active">
+							<a class="nav-link" href="services.php">Services</a>
+						</li>
+						<li class="dropdown nav-item">
+							<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">Pages
+								<b class="caret"></b>
+							</a>
+							<ul class="dropdown-menu agile_short_dropdown">
+								<li>
+									<a href="error.php">Error Page</a>
+								</li>
+								<li>
+									<a href="single.php">Single Page</a>
+								</li>
+							</ul>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="projects.php">Projects</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="contact.php">Contact</a>
+						</li>
+					</ul>
+				</div>
+			  
+			</nav>
+		</div>
+	</header>
+	<!--Header-->
+</div>
+<!-- //banner --> 
+
+<!-- services -->
+<section class="services py-5">
+	<div class="container py-lg-5 py-3">
+		<h2 class="heading text-capitalize mb-sm-5 mb-4"> Our Services </h2>
+		<div class="row service-grids">
+			<div class="col-lg-4 col-md-6 service-grid1">
+				<h3>Premium Grade Quality</h3>
+				<div class="row">
+					<div class="col-md-3 col-2">
+						<h4>1</h4>
+					</div>
+					<div class="col-md-9 col-10">
+						<p>Phasellus iaculis sapien in tellus gravida, a placerat lacus elementum. Nulla vitae lacus nec elit mollis pretium.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 mt-md-0 mt-5 service-grid1">
+				<h3>Handmade Collections</h3>
+				<div class="row">
+					<div class="col-md-3 col-2">
+						<h4>2</h4>
+					</div>
+					<div class="col-md-9 col-10">
+						<p>Phasellus iaculis sapien in tellus gravida, a placerat lacus elementum. Nulla vitae lacus nec elit mollis pretium.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 mt-lg-0 mt-5 service-grid1">
+				<h3>Soft Furniture</h3>
+				<div class="row">
+					<div class="col-md-3 col-2">
+						<h4>3</h4>
+					</div>
+					<div class="col-md-9 col-10">
+						<p>Phasellus iaculis sapien in tellus gravida, a placerat lacus elementum. Nulla vitae lacus nec elit mollis pretium.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 mt-5 service-grid1">
+				<h3>Comfortable Chairs</h3>
+				<div class="row">
+					<div class="col-md-3 col-2">
+						<h4>4</h4>
+					</div>
+					<div class="col-md-9 col-10">
+						<p>Phasellus iaculis sapien in tellus gravida, a placerat lacus elementum. Nulla vitae lacus nec elit mollis pretium.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 mt-5 service-grid1">
+				<h3>Outdoor Furniture</h3>
+				<div class="row">
+					<div class="col-md-3 col-2">
+						<h4>5</h4>
+					</div>
+					<div class="col-md-9 col-10">
+						<p>Phasellus iaculis sapien in tellus gravida, a placerat lacus elementum. Nulla vitae lacus nec elit mollis pretium.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 mt-5 service-grid1">
+				<h3>Office Furniture</h3>
+				<div class="row">
+					<div class="col-md-3 col-2">
+						<h4>6</h4>
+					</div>
+					<div class="col-md-9 col-10">
+						<p>Phasellus iaculis sapien in tellus gravida, a placerat lacus elementum. Nulla vitae lacus nec elit mollis pretium.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- services -->
+
+<!-- banner-bottom -->
+	<div class="banner-bottom">
+		<div class="container-fluid">
+			<div class="banner_bottom_agile_grids">
+				<div class="row wthree_banner_bottom_right_grids pr-sm-3">
+					<div class="col-lg-3 col-sm-6 banner_bottom_right_grid">
+						<div class="view view-tenth">
+							<div class="agile_text_box">
+								<i class="fas fa-bath" aria-hidden="true"></i>
+								<h3> Bedroom Design</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adip. Sed semper sem non commodo egestas. In rutrum enim a neque volutpat aliquet</p>
+							</div>
+							<div class="mask">
+								<img src="images/s1.jpg" class="img-responsive" alt="" />
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6 mt-sm-0 mt-3 banner_bottom_right_grid">
+						<div class="view view1 view-tenth">
+							<div class="agile_text_box">
+								<i class="fas fa-bed" aria-hidden="true"></i>
+								<h3> Kitchen Design</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adip. Sed semper sem non commodo egestas. In rutrum enim a neque volutpat aliquet</p>
+							</div>
+							<div class="mask">
+								<img src="images/s2.jpg" class="img-responsive" alt="" />
+							</div>
+						</div>
+					</div>
+				
+					<div class="col-lg-3 col-sm-6 mt-lg-0 mt-3 banner_bottom_right_grid">
+						<div class="view view2 view-tenth">
+							<div class="agile_text_box">
+								<i class="fas fa-bath" aria-hidden="true"></i>
+								<h3>Home Furniture </h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adip. Sed semper sem non commodo egestas. In rutrum enim a neque volutpat aliquet</p>
+							</div>
+							<div class="mask">
+								<img src="images/s3.jpg" class="img-responsive" alt="" />
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6 mt-lg-0 mt-3 banner_bottom_right_grid">
+						<div class="view view3 view-tenth">
+							<div class="agile_text_box">
+								<i class="fas fa-bed" aria-hidden="true"></i>
+								<h3>Office Chairs</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adip. Sed semper sem non commodo egestas. In rutrum enim a neque volutpat aliquet</p>
+							</div>
+							<div class="mask">
+								<img src="images/s4.jpg" class="img-responsive" alt="" />
+							</div>
+						</div>
+					</div>
+					<div class="clearfix"> </div>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+</div>
+<!-- banner-bottom -->
+
+
+<!-- team -->
+<section class="w3ls-team py-5">
+	<div class="container py-3">
+		<h3 class="heading text-capitalize mb-sm-5 mb-4"> Our Team </h3>
+		<div class="row team-grids">
+			<div class="col-md-3 col-sm-6 w3_agileits-team1">
+				<img class="img-fluid" src="images/t1.jpg" alt="">
+				<h5 class="mt-3">Elmore</h5>
+				<p>Lorem ipsum</p>
+				<div class="social-icons mt-2">
+					<ul>
+						<li>
+							<a href="#" class="fab fa-facebook-f icon-border facebook"> </a>
+						</li>
+						<li class="mx-1">
+							<a href="#" class="fab fa-twitter  icon-border twitter"> </a>
+						</li>
+						<li>
+							<a href="#" class="fab fa-google-plus-g icon-border googleplus"> </a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6 mt-sm-0 mt-5 w3_agileits-team1">
+				<img class="img-fluid" src="images/t2.jpg" alt="">
+				<h5 class="mt-3">Blanton</h5>
+				<p>Lorem ipsum</p>
+				<div class="social-icons mt-2">
+					<ul>
+						<li>
+							<a href="#" class="fab fa-facebook-f icon-border facebook"> </a>
+						</li>
+						<li class="mx-1">
+							<a href="#" class="fab fa-twitter  icon-border twitter"> </a>
+						</li>
+						<li>
+							<a href="#" class="fab fa-google-plus-g icon-border googleplus"> </a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6 mt-md-0 mt-5 w3_agileits-team1">
+				<img class="img-fluid" src="images/t3.jpg" alt="">
+				<h5 class="mt-3"> Bass</h5>
+				<p>Lorem ipsum</p>
+				<div class="social-icons mt-2">
+					<ul>
+						<li>
+							<a href="#" class="fab fa-facebook-f icon-border facebook"> </a>
+						</li>
+						<li class="mx-1">
+							<a href="#" class="fab fa-twitter  icon-border twitter"> </a>
+						</li>
+						<li>
+							<a href="#" class="fab fa-google-plus-g icon-border googleplus"> </a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6 mt-md-0 mt-5 w3_agileits-team1">
+				<img class="img-fluid" src="images/t4.jpg" alt="">
+				<h5 class="mt-3"> Glickon</h5>
+				<p>Lorem ipsum</p>
+				<div class="social-icons mt-2">
+					<ul>
+						<li>
+							<a href="#" class="fab fa-facebook-f icon-border facebook"> </a>
+						</li>
+						<li class="mx-1">
+							<a href="#" class="fab fa-twitter  icon-border twitter"> </a>
+						</li>
+						<li>
+							<a href="#" class="fab fa-google-plus-g icon-border googleplus"> </a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- //team -->
+
+<!-- footer -->
+<footer class="py-5">
+	<div class="container py-md-5">
+		<div class="footer-logo mb-5 text-center">
+			<a class="navbar-brand" href="index.html">In <span class="display"> Trend</span></a>
+		</div>
+		<div class="footer-grid">
+			<div class="social mb-4 text-center">
+				<ul class="d-flex justify-content-center">
+					<li class="mx-2"><a href="#"><span class="fab fa-facebook-f"></span></a></li>
+					<li class="mx-2"><a href="#"><span class="fab fa-twitter"></span></a></li>
+					<li class="mx-2"><a href="#"><span class="fas fa-rss"></span></a></li>
+					<li class="mx-2"><a href="#"><span class="fab fa-linkedin-in"></span></a></li>
+					<li class="mx-2"><a href="#"><span class="fab fa-google-plus"></span></a></li>
+				</ul>
+			</div>
+			<div class="list-footer">
+				<ul class="footer-nav text-center">
+					<li>
+						<a href="index.html">Home</a>
+					</li>
+					<li>
+						<a href="about.html">About</a>
+					</li>
+					<li>
+						<a href="services.html">Services</a>
+					</li>
+					<li>
+						<a href="projects.html">Gallery</a>
+					</li>
+					<li>
+						<a href="contact.html">Contact</a>
+					</li>
+				</ul>
+			</div>
+			<div class="agileits_w3layouts-copyright mt-4 text-center">
+				<p>© 2018 Intrend. All Rights Reserved | Design by <a href="http://w3layouts.com/" target="=_blank"> W3layouts </a></p>
+		</div>
+		</div>
+	</div>
+</footer>
+<!-- footer -->
+
+<!-- js-scripts -->		
+
+	<!-- js -->
+	<script type="text/javascript" src="js/jquery-2.2.3.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script> <!-- Necessary-JavaScript-File-For-Bootstrap --> 
+	<!-- //js -->
+	
+	<!-- banner js -->
+	<script src="js/snap.svg-min.js"></script>
+	<script src="js/main.js"></script> <!-- Resource jQuery -->  
+	<!-- //banner js --> 
+
+	<!-- flexSlider --><!-- for testimonials -->
+	<script defer src="js/jquery.flexslider.js"></script>
+	<script type="text/javascript">
+		$(window).load(function(){
+		  $('.flexslider').flexslider({
+			animation: "slide",
+			start: function(slider){
+			  $('body').removeClass('loading');
+			}
+		  });
+		});
+	</script>
+	<!-- //flexSlider --><!-- for testimonials -->
+
+	<!-- start-smoth-scrolling -->
+	<script src="js/SmoothScroll.min.js"></script>
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<script type="text/javascript" src="js/easing.js"></script>
+	<script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event){		
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+			});
+		});
+	</script>
+	<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			*/
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+	<!-- //here ends scrolling icon -->
+	<!-- start-smoth-scrolling -->
+	
+<!-- //js-scripts -->
+
+</body>
+</html>
