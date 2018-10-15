752c9a82cc707d057ed986520ed8c25e9cc40050
Bharat Reddy <bharat8apr@gmail.com>
2018-10-15 14:49:03 +0530

    Profile Page Merged with homepage

--- home.php
+++ home.php
@@ -1,29 +1,4 @@
 <?php
- /*require 'includes/config.inc.php';
-
-  echo "<h1>Home Page</h1>";
-  echo '<form action="includes/logout.inc.php" method="post">
-    <button type="submit" name="logout-submit">Logout</button>
-  </form>';*/
-  ?><!--
-  <!DOCTYPE html>
-  <html lang="en" dir="ltr">
-    <head>
-      <meta charset="utf-8">
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-      <title></title>
-    </head>
-    <body>
-      <a href="profile.php"> <button type="button" class="btn btn-primary">
-        <?php echo $_SESSION['fname']; ?>
-      </button> </a>
-    </body>
-  </html>-->
-<?php
 
  require 'includes/config.inc.php';
 ?>
@@ -118,7 +93,7 @@
 						</a>
 						<ul class="dropdown-menu agile_short_dropdown">
 							<li>
-								<a href="error.php">My Profile</a>
+								<a href="profile.php">My Profile</a>
 							</li>
 							<li>
 								<a href="includes/logout.inc.php">Logout</a>
--- profile.php
+++ profile.php
@@ -20,10 +20,103 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <link rel="stylesheet" href="web_profile/css/smoothbox.css" type='text/css' media="all" />
 <link href="//fonts.googleapis.com/css?family=Pathway+Gothic+One" rel="stylesheet">
 <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+
+<script type="application/x-javascript">
+	addEventListener("load", function () {
+		setTimeout(hideURLbar, 0);
+	}, false);
+
+	function hideURLbar() {
+		window.scrollTo(0, 1);
+	}
+</script>
+<!--// Meta tag Keywords -->
+
+<link href="web_home/css_home/slider.css" type="text/css" rel="stylesheet" media="all">
+
+<!-- css files -->
+<link rel="stylesheet" href="web_home/css_home/bootstrap.css"> <!-- Bootstrap-Core-CSS -->
+<link rel="stylesheet" href="web_home/css_home/style.css" type="text/css" media="all" /> <!-- Style-CSS -->
+<link rel="stylesheet" href="web_home/css_home/fontawesome-all.css"> <!-- Font-Awesome-Icons-CSS -->
+<!-- //css files -->
+
+<!-- testimonials css -->
+<link rel="stylesheet" href="web_home/css_home/flexslider.css" type="text/css" media="screen" property="" /><!-- flexslider css -->
+<!-- //testimonials css -->
+
+<!-- web-fonts -->
+<link href="//fonts.googleapis.com/css?family=Poiret+One&amp;subset=cyrillic,latin-ext" rel="stylesheet">
+<!-- //web-fonts -->
+
+
 </head>
 <body>
+	<!-- banner -->
+		<div class="banner" id="home">
+			<div class="cd-radial-slider-wrapper">
+
+	<!--Header-->
+	<header>
+		<div class="container agile-banner_nav">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+				<h1><a class="navbar-brand" href="home.php">In <span class="display"> Trend</span></a></h1>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item active">
+							<a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="about.php">About</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="services.php">Hostels</a>
+						</li>
+						<li class="dropdown nav-item">
+							<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">Pages
+								<b class="caret"></b>
+							</a>
+							<ul class="dropdown-menu agile_short_dropdown">
+								<li>
+									<a href="error.php">Error Page</a>
+								</li>
+								<li>
+									<a href="single.php">Single Page</a>
+								</li>
+							</ul>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="projects.php">Notice Boards</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="contact.php">Contact</a>
+						</li>
+						<li class="dropdown nav-item">
+							<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown"><?php echo $_SESSION['roll']; ?>
+								<b class="caret"></b>
+							</a>
+							<ul class="dropdown-menu agile_short_dropdown">
+								<li>
+									<a href="profile.php">My Profile</a>
+								</li>
+								<li>
+									<a href="includes/logout.inc.php">Logout</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+
+			</nav>
+		</div>
+	</header>
+	<!--Header-->
+<br><br><br><br><br>
 	<div class="main">
-		<h1>Profile Page</h1>
 		<div id="navigation" style="display:none;" class="w3_agile">
 			<ul>
 				<li class="selected">
