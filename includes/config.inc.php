4bcebb666b7195320fdf3e6dd4c2e3ee5064c3a6
rakeshy1116 <yrakeshchowdary1116@gmail.com>
2018-10-20 14:46:12 +0530

    Modified config.inx.php for localhost

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,10 +1,10 @@
 <?php
   session_start();
-  $servername = "hms.test"; //change this  accordingly
+  $servername = "localhost"; //change this  accordingly
   $dBUsername = "root";
-  $dBPassword = "root";
+  $dBPassword = "";
   $dBName = "hostel_management_system";
-  session_start();
+ // session_start();
   $conn=mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
   if (!$conn) {
